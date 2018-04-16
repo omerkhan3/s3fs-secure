38a7dc5a093f45be236e2ef5bf85f2b24910d108
Omer Khan <okhan320@gmail.com>
2018-04-16 00:31:48 -0400

    Standalone updated
    Now includes key in terminal command, and lets user specify input/output files

--- standaloneRC4Utility/standaloneRC4.cpp
+++ standaloneRC4Utility/standaloneRC4.cpp
@@ -1,12 +1,10 @@
-
 #include <iostream>
 #include <string>
 #include <fstream>
 #include "rc4_enc.c"
 #include "rc4_skey.c"
 using namespace std;
-#define KEY "default_key"
-#define KEY_LENGTH (sizeof(KEY)-1)
+
 
 // Encrypt/decrypt function
 void encryptDecrypt(int argc, char *argv[]); 
@@ -14,13 +12,31 @@ void encryptDecrypt(int argc, char *argv[]);
 //Main
 int main(int argc, char *argv[]){
 	
-	if(argc > 3){
-		cerr << "Error! Too many file names given.";
+	if(argc > 8){
+
+		cerr << "Too many file names given: " << argc << endl;
 		return 0;}
-	else if(argc < 2){
-		cerr << "Error!  You need to provide a file name.";
+	else if(argc < 6){
+		cerr << "You must provide a file name." << endl;
 		return 0;
 	}
+	else if (strcmp(argv[1], "-e") != 0 && strcmp(argv[1], "-d") != 0)
+	{
+		cerr << "You must choose either -d or -e" << endl;
+		return 0;
+	}
+	else if (strcmp(argv[2], "-k") != 0)
+	{
+		cerr << "You must provide a key value." << endl;
+		return 0;
+	}
+
+	else if (strcmp(argv[4], "-in") != 0)
+	{
+		cerr << "Please provide an input file." << endl;
+		return 0;
+	}
+
 	else{
 		encryptDecrypt(argc, argv);
 		return 0;
@@ -28,18 +44,25 @@ int main(int argc, char *argv[]){
 }
 
 void encryptDecrypt(int argc, char* argv[]){
+
+
 	printf("Args: %d\n", argc);
+	ifstream pass;
+	pass.open("RC4_Key.txt"); 
+		printf("Pass: yes\n");
+		string password;
+		getline(pass,password);
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	        //Input and output filestreams
 	        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	        ifstream ifs(argv[1]);
+	        ifstream ifs(argv[5]);
 	        string input((istreambuf_iterator<char>(ifs)),
 	                (istreambuf_iterator<char>()));
 	        ofstream output;
-	        if(argc == 2){ //Single file for encrypt/decrypt
-	                output.open(argv[1], ios::binary);}
+	        if(argc == 6){ //Single file for encrypt/decrypt
+	                output.open(argv[5], ios::binary);}
 	        else{ //Separate file to encrypt/decrypt to
-	                output.open(argv[2], ios::binary);}
+	                output.open(argv[7], ios::binary);}
 	        RC4_KEY key;
 	        int length = input.size();
 	        unsigned char *buffer = (unsigned char*)malloc(length + 1);
@@ -47,7 +70,7 @@ void encryptDecrypt(int argc, char* argv[]){
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                 //Encryption
                 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-                RC4_set_key(&key, KEY_LENGTH, (const unsigned char*)KEY);
+                RC4_set_key(&key, strlen(argv[3]) - 1, (const unsigned char*)argv[3]);
                 RC4(&key, length, (const unsigned char*)input.c_str(), buffer);
                 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
                 //Write to file
@@ -59,4 +82,3 @@ void encryptDecrypt(int argc, char* argv[]){
                 output.close();
 
             }
-
